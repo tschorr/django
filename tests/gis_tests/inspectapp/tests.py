c17a0f10bc745df16ad3f94fd7eb9fa2d0e75975
Thomas Schorr <info@thomasschorr.de>
2017-09-19 11:07:28 +0000

    add test for mapping option

--- tests/gis_tests/inspectapp/tests.py
+++ tests/gis_tests/inspectapp/tests.py
@@ -151,6 +151,25 @@ class OGRInspectTest(TestCase):
         output = out.getvalue()
         self.assertIn('class City(models.Model):', output)
 
+    def test_mapping_option(self):
+        expected = \
+            """    geom = models.PointField(srid=-1)\n""" \
+            """\n""" \
+            """\n""" \
+            """# Auto-generated `LayerMapping` dictionary for City model\n""" \
+            """city_mapping = {\n""" \
+            """    'name': 'Name',\n""" \
+            """    'population': 'Population',\n""" \
+            """    'density': 'Density',\n""" \
+            """    'created': 'Created',\n""" \
+            """    'geom': 'POINT',\n""" \
+            """}\n"""
+        shp_file = os.path.join(TEST_DATA, 'cities', 'cities.shp')
+        out = StringIO()
+        call_command('ogrinspect', shp_file, '--mapping', 'City', stdout=out)
+        output = out.getvalue()
+        self.assertIn(expected, output)
+
 
 def get_ogr_db_string():
     """
